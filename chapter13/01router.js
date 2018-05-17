5de315b5d2e1c0e3f32311c5a875ba92d6a76e5f
wangwen <daydayup1991@>
2018-05-17 16:34:52 +0800

    Push to Remote Repository Test

--- chapter13/01router.js
+++ chapter13/01router.js
@@ -1,88 +1,72 @@
-
 //引入http模块
-var http=require('http');
+var http = require('http');
 
-var url=require('url');
+var url = require('url');
 
-var ejs=require('ejs');
+var ejs = require('ejs');
 
-var fs=require('fs');
+var fs = require('fs');
 
 //路由:指的就是针对不同请求的 URL，处理不同的业务逻辑。
-http.createServer(function(req,res){
+http.createServer(function (req, res) {
 
-	res.writeHead(200,{"Content-Type":"text/html;charset='utf-8'"});
-
-
-	//获取get 还是post请求
+    res.writeHead(200, {"Content-Type": "text/html;charset='utf-8'"});
 
 
-	var method=req.method.toLowerCase();
-	//console.log(method);
+    //获取get 还是post请求
+    var method = req.method.toLowerCase();
+    //console.log(method);
 
-	var pathname=url.parse(req.url,true).pathname;
+    var pathname = url.parse(req.url, true).pathname;
 
+    /*显示登录页面*/
+    if (pathname == '/login') {
 
-	if(pathname=='/login'){  /*显示登录页面*/
+        ejs.renderFile('views/form.ejs', {}, function (err, data) {
 
+            res.end(data);
 
-		ejs.renderFile('views/form.ejs',{
+        });
 
-		},function(err,data){
+    } else if (pathname == '/dologin' && method == 'get') {  /*执行登录的操作*/
+        //get获取数据
+        console.log(url.parse(req.url, true).query);
 
+        res.end('dologin');
 
-			res.end(data);
+    } else if (pathname == '/dologin' && method == 'post') {  /*执行登录的操作*/
+        var postStr = '';
+        req.on('data', function (chunk) {
 
-		})
+            postStr += chunk;
+        })
+        req.on('end', function (err, chunk) {
 
+            //res.end(postStr);
+            console.log(postStr);
 
-	}else if(pathname=='/dologin' &&method=='get'){  /*执行登录的操作*/
+            fs.appendFile('login.txt', postStr + '\n', function (err) {
 
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('写入数据成功');
+            })
 
-		//get获取数据
-		console.log(url.parse(req.url,true).query);
+            res.end("<script>alert('登录成功');history.back();</script>")
 
-		res.end('dologin');
-
-
-
-	}else if(pathname=='/dologin' &&method=='post'){  /*执行登录的操作*/
-
-
-		var postStr='';
-		req.on('data',function(chunk){
-
-			postStr+=chunk;
-		})
-		req.on('end',function(err,chunk){
-
-			//res.end(postStr);
-			console.log(postStr);
-
-			fs.appendFile('login.txt',postStr+'\n',function(err){
-
-				if(err){
-					console.log(err);
-					return;
-				}
-				console.log('写入数据成功');
-			})
-
-			res.end("<script>alert('登录成功');history.back();</script>")
-
-		})
+        })
 
 
-	}else{
+    } else {
 
-		ejs.renderFile('views/index.ejs',{
+        ejs.renderFile('views/index.ejs', {}, function (err, data) {
 
-		},function(err,data){
+            res.end(data);
 
-			res.end(data);
-
-		})
-	}
+        })
+    }
 
 }).listen(8001);
 
